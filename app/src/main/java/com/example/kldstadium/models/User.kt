e42a673b3e10430796fac78ad9683e3cff47be55
KirsonBrz <kirill0603@gmail.com>
2021-09-10 16:13:59 +0300

    Add TabLayout with car/on foot choose

--- app/src/main/java/com/example/kldstadium/models/User.kt
+++ app/src/main/java/com/example/kldstadium/models/User.kt
@@ -1,9 +1,14 @@
 package com.example.kldstadium.models
 
+import com.google.firebase.Timestamp
+import java.util.*
+
 data class User(
     val id: String? = null,
-    val name: Double? = null,
-    val date: Double? = null,
+    val name: String? = null,
+    val date: Date? = null,
+    val state: Int? = null,
     //val avatar: String? = null,
-    val purpose: Double? = null
+    val purpose: String? = null
 )
+
